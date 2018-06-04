ce9a1410cffd80742087d367b9d64fa8ee053172
Sam <sambsnyder@gmail.com>
2018-06-03 23:19:22 -0700

    broken - added circle via two but undefined symbols abound

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,5 +1,22 @@
 package com.github.kiddaedalus.presentation
 
+import org.w3c.dom.HTMLElement
+import kotlin.browser.document
+
 fun main(vararg args: String) {
     console.log("Hello console from kotlin")
+    val drawShapes = document.getElementById("draw-shapes") as HTMLElement
+    val params = object : TwoConstructionParams {
+        override var width: Number? = 200
+        override var height: Number? = 200
+    }
+    val two = Two(params)
+    two.appendTo(drawShapes)
+
+    val circle = two.makeCircle(50, 50, 100).apply {
+        fill = "#FF8000"
+        stroke = "orangered"
+        linewidth = 5
+    }
+    two.update()
 }
